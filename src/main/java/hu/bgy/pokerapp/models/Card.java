f81fa9dd297fa240e014c9a2db29e9e73be85db0
Gyorgy <gyorgybozsik87@gmail.com>
2024-04-05 13:39:03 +0200

    Card clean up

--- src/main/java/hu/bgy/pokerapp/models/Card.java
+++ src/main/java/hu/bgy/pokerapp/models/Card.java
@@ -4,21 +4,9 @@ import hu.bgy.pokerapp.enums.Rank;
 import hu.bgy.pokerapp.enums.Symbol;
 import lombok.NonNull;
 
-import java.util.Comparator;
-
 public record Card(Symbol symbol,
                    Rank rank) implements Comparable<Card> {
 
-
-
-
-
-   // public Card(int symbol, int rank) {
-   //     Symbol[] symbols = Symbol.values();
-   //     Rank[] ranks = Rank.values();
-   //     this.rank = ranks[rank];
-   //     this.symbol = symbols[symbol];
-   // }
     public boolean isHighest() {
         return rank.isHighest();
     }
@@ -28,24 +16,11 @@ public record Card(Symbol symbol,
     }
 
     @Override
-    public int compareTo(Card o) {
-        //final int diff =  this.rank.ordinal() - o.rank.ordinal();
+    public int compareTo(@NonNull final Card o) {
         if (this.rank.compareTo(o.rank) == 0) {
             return this.symbol.compareTo(o.symbol);
         } else {
             return this.rank.compareTo(o.rank);
         }
-
-        //return Comparator.comparing(rank.ordinal());
-    }
-    //  @Override
-    //  public int compareTo(Card o) {
-    //      return this.rank.ordinal() - o.rank.ordinal();
-    //  }
-
-    final static Comparator<Card> cardComparator = Comparator.comparing(Card::rank).thenComparing(Card::symbol);
-
-    public String print() {
-        return symbol.name() + " " + rank.name();
     }
 }
